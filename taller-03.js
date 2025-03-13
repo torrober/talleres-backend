5881ab2ab63cd5194413ebc5750b8735d65af821
Roberto M. Rocha <robemario124@gmail.com>
2025-03-13 15:29:16 -0500

    funcion desglosarstring

--- taller-03.js
+++ taller-03.js
@@ -1,3 +1,20 @@
 function desglosarString(str, tipo) {
-    return 0;
+    const vocales = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    const lowerStr = str.toLowerCase();
+    
+    for (let i = 0; i < lowerStr.length; i++) {
+        const char = lowerStr[i];
+        if (vocales.includes(char) && tipo === 'vocales') {
+            count++;
+        } else if (
+            tipo === 'consonantes' &&
+            char >= 'a' && 
+            char <= 'z' && 
+            !vocales.includes(char)
+        ) {
+            count++;
+        }
+    }
+    return count;
 }

b912d6e2673407bb2dbbcb16587f0af6b3e0d356
torrober <robemario124@gmail.com>
2025-04-04 14:30:53 -0500

    excersises 1-3 + ts reconfig

--- taller-04/src/user/v1/user.controller.ts
+++ taller-04/src/user/v1/user.controller.ts
@@ -1,12 +1,35 @@
-import readUserAction from "./read.user.action";
-import { UserType } from "./user.model";
-
+import { Request, Response } from 'express';
+import data from '../../24-taller-04-datos.json'; 
+import { UserModel } from './user.model';
 // DECLARE CONTROLLER FUNCTIONS
-async function readUsers(): Promise<UserType[]> {
-  const users = await readUserAction();
+export const getUsersByHobby: any = (req: Request, res: Response) => {
+  const { hobby } = req.params; 
+  const usersWithHobby = data.filter(user => user.hobbies.includes(hobby));
 
-  return users;
+  if (usersWithHobby.length > 0) {
+      res.json(usersWithHobby); 
+  } else {
+      res.status(404).json({ message: 'No hay usuarios con ese hobby' }); 
+  }
 }
+export const userExists = (req: Request, res: Response) => {
+  const { codigo } = req.params; 
+  const user = data.find(user => parseInt(user.codigo) === parseInt(codigo)); 
 
-// EXPORT CONTROLLER FUNCTIONS
-export { readUsers };
+  if (user) {
+      res.json({ exists: true }); 
+  } else {
+      res.json({ exists: false }); 
+  }
+};
+export const getUserCountByHobby = (req: Request, res: Response) => {
+  const { hobby } = req.params;
+  const usersWithHobby = data.filter(user => user.hobbies.includes(hobby));
+  
+  res.json({
+      hobby,
+      count: usersWithHobby.length
+  });
+};
+
+

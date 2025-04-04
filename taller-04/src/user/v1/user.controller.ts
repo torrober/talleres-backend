9afa6cbc5a84cc9827079b415c718b08df851e7f
torrober <robemario124@gmail.com>
2025-04-04 14:48:37 -0500

    excersises 4-6

--- taller-04/src/user/v1/user.controller.ts
+++ taller-04/src/user/v1/user.controller.ts
@@ -32,4 +32,55 @@ export const getUserCountByHobby = (req: Request, res: Response) => {
   });
 };
 
+export const getFreeTimeUsers = (req: Request, res: Response) => {
+  const usersWithFreeTime = data.filter(user => user.hobbies.length < 3);
 
+  if (usersWithFreeTime.length > 0) {
+      res.json(usersWithFreeTime);
+  } else {
+      res.status(404).json({ message: 'No hay usuarios con tiempo libre' });
+  }
+};
+export const addHobbyToUser = (req: Request, res: Response) => {
+  const { codigo } = req.params;
+  const { hobby } = req.body;
+
+  const userIndex = data.findIndex(user => user.codigo === codigo);
+  
+  if (userIndex === -1) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
+  }
+
+  const user = data[userIndex];
+  
+  if (user.hobbies.length >= 3) {
+      return res.status(400).json({ 
+          message: 'El usuario ya tiene 3 hobbies, no se pueden agregar más' 
+      });
+  }
+
+  if (user.hobbies.includes(hobby)) {
+      return res.status(400).json({ 
+          message: 'El usuario ya tiene este hobby' 
+      });
+  }
+
+  user.hobbies.push(hobby);
+  res.json({ 
+      message: 'Hobby agregado exitosamente',
+      user 
+  });
+};
+export const registerUser = (req: Request, res: Response) => {
+  const { codigo, nombre, apellido, hobbies } = req.body; 
+  if (!codigo || !nombre || !apellido || !hobbies) {
+      return res.status(400).json({ message: "Todos los campos son requeridos" });
+  } else {
+    if(hobbies.length<2) {
+      return res.status(400).json({ message: "El usuario no puede tener menos de 2 hobbies" });
+    } else {
+      const newUser = { codigo, nombre, apellido, hobbies };
+      res.status(201).json(newUser); 
+    }
+  }
+};
